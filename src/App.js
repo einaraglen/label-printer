ff56fe00d17a4c1a221b78e88f3a719abce4b7b9
Einar <einar.aglen99@gmail.com>
2021-08-02 20:39:32 +0200

    Printing now works when called from renderer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ const parser = window.require("fast-xml-parser");
 const { ipcRenderer } = window.require('electron');
 
 const App = () => {
+    //here we define what we want to extract from the IFS XML
+    const keyValues = ["PartNo", "PartDescription", "SubProjectID", "ProjectID", "OnHandQty"]
     //makes it so we can get our data async
     const readFile = async (path) => {
         return new Promise((resolve, reject) => {
@@ -23,7 +25,7 @@ const App = () => {
         let rawTemplate = await readFile("./src/template/xml-template.xml");
         let template = rawTemplate.toString();
         //get the data rows
-        let rawData = await readFile("./src/test/test1.xml");
+        let rawData = await readFile("./src/test/test.xml");
         let data = [];
         //convert XML into JSON
         let json = parser.parse(rawData.toString());
@@ -33,11 +35,10 @@ const App = () => {
         data = (!toBePrinted.length) ? [toBePrinted] : [...toBePrinted]
         //start print process
         data.forEach((data) => {
-            for (const key in data) {
+            keyValues.forEach((key) => {
                 let regex = new RegExp(key, "g");
-                if (template.indexOf(key) > -1)
-                    template = template.toString().replace(regex, data[key]);
-            }
+                template = template.toString().replace(regex, data[key]);
+            });
             //listen for response from main process
             ipcRenderer.on('print-response', (event, arg) => {
                 console.log(arg); // prints "Hello World!"
