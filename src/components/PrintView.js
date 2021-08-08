cb334893d90be309c99a42614cdaffc42a7f49d2
Einar <einar.aglen99@gmail.com>
2021-08-08 23:42:42 +0200

    Trying carousel for previews!

--- src/components/PrintView.js
+++ src/components/PrintView.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Button } from "@material-ui/core";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { Context } from "context/State";
+import { AutoRotatingCarousel } from "material-auto-rotating-carousel";
 
 //we can now amazingly access awsome shit in our render!
 const fs = window.require("fs");
@@ -10,6 +11,7 @@ const { ipcRenderer } = window.require("electron");
 const path = window.require("path");
 
 const PrintView = () => {
+    const [images, setImages] = React.useState([]);
     const [imagePreview, setImagePreview] = React.useState(null);
     const [isLoading, setIsLoading] = React.useState(false);
     const [labels, setLabels] = React.useState([]);
@@ -28,28 +30,50 @@ const PrintView = () => {
         const loadData = async () => {
             let result = await ipcRenderer.invoke("get-file");
             //for testing
-            result = !result ? `./src/test/${paths[0]}` : result;
+            result = !result ? `./src/test/${paths[2]}` : result;
             const fileName = path.parse(result).base.toString().split(" ")[0];
-            let currentConfig = getCurrentConfig(stateRef.current.value.config, fileName);
+            let currentConfig = getCurrentConfig(
+                stateRef.current.value.config,
+                fileName
+            );
             const rawData = await readFile(result);
             const data = parser.parse(rawData);
             const rows = data.Table.Row;
             let currentData = !rows.length ? [rows] : [...rows];
             let tempPath = await ipcRenderer.invoke("get-template");
             //build print data for dymo printer
-            let currentLabels = buildLabels(currentData, currentConfig, await readFile(tempPath), fileName);
+            let currentLabels = buildLabels(
+                currentData,
+                currentConfig,
+                await readFile(tempPath),
+                fileName
+            );
             //preview first label
-            if (!currentLabels[0]) return;
-            let preview = await ipcRenderer.invoke("image-preview", currentLabels[0]);
-            setImagePreview(preview.replace(/"/g, ""));
+            if (!currentLabels) return;
+            setImages(await getImages(currentLabels));
             setLabels([...currentLabels]);
         };
 
         loadData();
     }, []);
 
+    //has to be called async
+    const getImages = async (currentLabels) => {
+        let images = [];
+        if (!currentLabels) return [];
+        for (let i = 0; i < currentLabels.length; i++) {
+            let preview = await ipcRenderer.invoke(
+                "image-preview",
+                currentLabels[i]
+            );
+            images.push(preview.replace(/"/g, ""));
+        }
+        return images;
+    };
+
     const getCurrentConfig = (config, fileName) => {
-        for (const property in config) if (fileName.indexOf(property) > -1) return config[property];
+        for (const property in config)
+            if (fileName.indexOf(property) > -1) return config[property];
     };
 
     const buildLabels = (currentData, currentConfig, templateXML, fileName) => {
@@ -63,10 +87,16 @@ const PrintView = () => {
                 //handle "LineInfo"
                 if (property === "LineInfo") {
                     //TODO: handle shit and return the string
-                    let lineInfo = handleLineInfo(currentProperty, fileName, current);
+                    let lineInfo = handleLineInfo(
+                        currentProperty,
+                        fileName,
+                        current
+                    );
                     labelXML = labelXML.toString().replace(regex, lineInfo);
                 } else {
-                    labelXML = labelXML.toString().replace(regex, current[currentConfig[property]]);
+                    labelXML = labelXML
+                        .toString()
+                        .replace(regex, current[currentConfig[property]]);
                 }
             }
             currentLabels.push(labelXML);
@@ -81,16 +111,23 @@ const PrintView = () => {
         for (let i = 0; i < currentProperty.length; i++) {
             let currentLine = handleLine(currentProperty[i], fileName, current);
             lineInfo += currentLine;
-            lineInfo += i === currentProperty.length - 1 || currentLine.length === 0 ? "" : " - ";
+            lineInfo +=
+                i === currentProperty.length - 1 || currentLine.length === 0
+                    ? ""
+                    : " - ";
         }
         return lineInfo;
     };
 
     const handleLine = (currentPropertyOf, fileName, current) => {
-        if (currentPropertyOf === "PurchaseOrder") return `PO ${fileName.replace("PurchaseOrder", "")}`;
-        if (currentPropertyOf === "CustomersPONo") return `PO ${current[currentPropertyOf]}`;
+        if (currentPropertyOf === "PurchaseOrder")
+            return `PO ${fileName.replace("PurchaseOrder", "")}`;
+        if (currentPropertyOf === "CustomersPONo")
+            return `PO ${current[currentPropertyOf]}`;
         if (currentPropertyOf === "ProjectID")
-            return current[currentPropertyOf].length !== 0 ? `P-${current[currentPropertyOf]}` : "";
+            return current[currentPropertyOf].length !== 0
+                ? `P-${current[currentPropertyOf]}`
+                : "";
         return current[currentPropertyOf];
     };
 
@@ -109,10 +146,18 @@ const PrintView = () => {
         for (let i = 0; i < labels.length; i++) {
             let currentLabel = labels[i].replace(/(\r\n|\n|\r)/gm, "");
             //set current preview
-            let preview = await ipcRenderer.invoke("image-preview", currentLabel);
-            setImagePreview(preview.replace(/"/g, ""));
-            state.method.setButtonText(`Printing Label ${i + 1} of ${labels.length}`);
-            let printResult = await ipcRenderer.invoke("print-label", currentLabel);
+            let preview = await ipcRenderer.invoke(
+                "image-preview",
+                currentLabel
+            );
+            //setImagePreview(preview.replace(/"/g, ""));
+            state.method.setButtonText(
+                `Printing Label ${i + 1} of ${labels.length}`
+            );
+            let printResult = await ipcRenderer.invoke(
+                "print-label",
+                currentLabel
+            );
             await new Promise((resolve) => setTimeout(resolve, 2000));
         }
         //complete print with close
@@ -125,14 +170,24 @@ const PrintView = () => {
     return (
         <>
             <div className="preview">
-                {!imagePreview ? null : (
-                    <img style={{ height: "8rem" }} alt="label preview" src={`data:image/png;base64,${imagePreview}`} />
-                )}
+                {/*<AutoRotatingCarousel open >
+                    {/*images.map((image) => (
+                        <img
+                            style={{ height: "8rem" }}
+                            alt="label preview"
+                            src={`data:image/png;base64,${image}`}
+                        />
+                    ))}
+                </AutoRotatingCarousel>*/}
             </div>
             <div className="print">
                 <Button
-                    onClick={!state.value.isTemplateGood || isLoading ? null : print}
-                    color={!state.value.isTemplateGood ? "secondary" : "primary"}
+                    onClick={
+                        !state.value.isTemplateGood || isLoading ? null : print
+                    }
+                    color={
+                        !state.value.isTemplateGood ? "secondary" : "primary"
+                    }
                     variant="outlined"
                     style={{ width: "20rem" }}
                     endIcon={isLoading ? <CircularProgress size={20} /> : null}
