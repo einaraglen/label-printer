34a40dffc8d00856dd362f709068a5051dee26e2
einar <aglen>
2021-08-12 14:02:18 +0200

    Added options for Singles
    When there only is one row with multiple qty!

--- src/components/PrintView.js
+++ src/components/PrintView.js
@@ -3,6 +3,8 @@ import { Button } from "@material-ui/core";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { Context } from "context/State";
 import LabelCarousel from "./LabelCarousel";
+import Switch from "@material-ui/core/Switch";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 //we can now amazingly access awsome shit in our render!
 const fs = window.require("fs");
@@ -15,52 +17,87 @@ const PrintView = ({ startPrint }) => {
     const [printIndex, setPrintIndex] = React.useState(null);
     const [isLoading, setIsLoading] = React.useState(false);
     const [labels, setLabels] = React.useState([]);
+    //for our lil special case
+    const [jsonData, setJsonData] = React.useState([]);
+    const [configInUse, setConfigInUse] = React.useState({});
+    const [singles, setSingles] = React.useState(false);
 
     const state = React.useContext(Context);
     const stateRef = React.useRef(state);
 
-    const handleLineInfo = React.useCallback((currentProperty, fileName, current) => {
-        let lineInfo = "";
-        for (let i = 0; i < currentProperty.length; i++) {
-            let currentLine = handleLine(currentProperty[i], fileName, current);
-            lineInfo += currentLine;
-            lineInfo +=
-                i === currentProperty.length - 1 || currentLine.length === 0
-                    ? ""
-                    : " - ";
-        }
-        return lineInfo;
-    }, []);
-
-    const buildLabels = React.useCallback((currentData, currentConfig, templateXML, fileName) => {
-        let currentLabels = [];
-        let labelXML = templateXML.toString();
-        for (let i = 0; i < currentData.length; i++) {
-            let current = currentData[i];
-            for (const property in currentConfig) {
-                const regex = new RegExp(property, "g");
-                const currentProperty = currentConfig[property];
-                //handle "LineInfo"
-                if (property === "LineInfo") {
-                    //TODO: handle shit and return the string
-                    let lineInfo = handleLineInfo(
-                        currentProperty,
-                        fileName,
-                        current
-                    );
-                    labelXML = labelXML.toString().replace(regex, lineInfo);
-                } else {
-                    labelXML = labelXML
-                        .toString()
-                        .replace(regex, current[currentConfig[property]]);
-                }
+    const handleLineInfo = React.useCallback(
+        (currentProperty, fileName, current) => {
+            let lineInfo = "";
+            for (let i = 0; i < currentProperty.length; i++) {
+                let currentLine = handleLine(
+                    currentProperty[i],
+                    fileName,
+                    current
+                );
+                lineInfo += currentLine;
+                lineInfo +=
+                    i === currentProperty.length - 1 || currentLine.length === 0
+                        ? ""
+                        : " - ";
             }
-            currentLabels.push(labelXML);
-            //reset the template string
-            labelXML = templateXML.toString();
+            return lineInfo;
+        },
+        []
+    );
+
+    const handleProperty = (
+        singles,
+        property,
+        fileName,
+        current,
+        labelXML,
+        currentConfig
+    ) => {
+        const regex = new RegExp(property, "g");
+        const currentProperty = currentConfig[property];
+        //handle "LineInfo"
+        if (property === "LineInfo") {
+            //TODO: handle shit and return the string
+            let lineInfo = handleLineInfo(currentProperty, fileName, current);
+            return labelXML.toString().replace(regex, lineInfo);
         }
-        return [...currentLabels];
-    }, [handleLineInfo]);
+        if (property === "LineQuantity") {
+            return labelXML
+                .toString()
+                .replace(regex, !singles ? `${current[currentConfig[property]]} pcs` : "1 pcs");
+        }
+        return labelXML
+            .toString()
+            .replace(regex, current[currentConfig[property]]);
+    };
+
+    const buildLabels = React.useCallback(
+        (singles, currentData, currentConfig, templateXML, fileName) => {
+            let currentLabels = [];
+            let limit = !singles
+                ? currentData.length
+                : currentData[0][currentConfig.LineQuantity];
+            let labelXML = templateXML.toString();
+            for (let i = 0; i < limit; i++) {
+                let current = !singles ? currentData[i] : currentData[0];
+                for (const property in currentConfig) {
+                    labelXML = handleProperty(
+                        singles,
+                        property,
+                        fileName,
+                        current,
+                        labelXML,
+                        currentConfig
+                    );
+                }
+                currentLabels.push(labelXML);
+                //reset the template string
+                labelXML = templateXML.toString();
+            }
+            return [...currentLabels];
+        },
+        [handleLineInfo]
+    );
 
     React.useEffect(() => {
         //test paths
@@ -68,6 +105,7 @@ const PrintView = ({ startPrint }) => {
             "CustomerOrderS16112 210804-110500.xml",
             "InventoryPartInStock 210802-155209.xml",
             "PurchaseOrder629195 210803-141357.xml",
+            "InventoryPartInStock 210812-124414.xml",
         ];
 
         const loadData = async () => {
@@ -75,21 +113,21 @@ const PrintView = ({ startPrint }) => {
             let config = await ipcRenderer.invoke("get-config");
 
             //for testing
-            result = !result ? `./src/test/${paths[2]}` : result;
+            result = !result ? `./src/test/${paths[3]}` : result;
             stateRef.current.method.setCurrentPath(result);
             const fileName = path.parse(result).base.toString().split(" ")[0];
-            let currentConfig = getCurrentConfig(
-                config,
-                fileName
-            );
+            let currentConfig = getCurrentConfig(config, fileName);
+            setConfigInUse(currentConfig);
             const rawData = await readFile(result);
             const data = parser.parse(rawData);
             const rows = data.Table.Row;
             let currentData = !rows.length ? [rows] : [...rows];
+            setJsonData(currentData);
             let tempPath = await ipcRenderer.invoke("get-template");
             if (!tempPath) return;
             //build print data for dymo printer
             let currentLabels = buildLabels(
+                singles,
                 currentData,
                 currentConfig,
                 await readFile(tempPath),
@@ -102,7 +140,7 @@ const PrintView = ({ startPrint }) => {
         };
 
         loadData();
-    }, [buildLabels]);
+    }, [buildLabels, singles]);
 
     //has to be called async
     const getImages = async (currentLabels) => {
@@ -147,6 +185,7 @@ const PrintView = ({ startPrint }) => {
 
     const print = async () => {
         setIsLoading(true);
+        //this calles the start printing method from App.js
         startPrint();
         for (let i = 0; i < labels.length; i++) {
             let currentLabel = labels[i].replace(/(\r\n|\n|\r)/gm, "");
@@ -154,10 +193,7 @@ const PrintView = ({ startPrint }) => {
             state.method.setButtonText(
                 `Printing Label ${i + 1} of ${labels.length}`
             );
-            await ipcRenderer.invoke(
-                "print-label",
-                currentLabel
-            );
+            await ipcRenderer.invoke("print-label", currentLabel);
             await new Promise((resolve) => setTimeout(resolve, 2000));
         }
         //complete print with close
@@ -165,6 +201,17 @@ const PrintView = ({ startPrint }) => {
         //setIsLoading(false);
         await new Promise((resolve) => setTimeout(resolve, 2000));
         await ipcRenderer.invoke("complete");
+    };
+
+    const specialCase = () => {
+        //if there is only 1 labels, with more that 1 qty
+        return (
+            jsonData.length === 1 && jsonData[0][configInUse.LineQuantity] > 1
+        );
+    };
+
+    const toggleSingles = (event) => {
+        setSingles(!singles);
     };
 
     return (
@@ -177,6 +224,20 @@ const PrintView = ({ startPrint }) => {
                 />
             </div>
             <div className="print">
+                <div className="controlls">
+                    <FormControlLabel
+                        control={
+                            <Switch
+                                disabled={!specialCase()}
+                                checked={singles}
+                                onChange={toggleSingles}
+                                color="primary"
+                            />
+                        }
+                        label="Singles"
+                        labelPlacement="start"
+                    />
+                </div>
                 <Button
                     onClick={
                         !state.value.isTemplateGood || isLoading ? null : print
@@ -185,7 +246,7 @@ const PrintView = ({ startPrint }) => {
                         !state.value.isTemplateGood ? "secondary" : "primary"
                     }
                     variant="outlined"
-                    style={{ width: "20rem" }}
+                    style={{ width: "56%" }}
                     endIcon={isLoading ? <CircularProgress size={20} /> : null}
                 >
                     {state.value.buttonText}
